468e9b02da6d17067ce2f2f44ddb363640ea9311
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2023-09-27 07:49:36 -0400

    Properly sends emails via MailJet

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -19,9 +19,50 @@ export class EmailService {
 	
 
 	async sendEmail(bodyData) {
+		const Mailjet = require('node-mailjet');
 
-		console.log('hits endpoint with: ' + bodyData.name, bodyData.email, bodyData.emailBody)
-		return;
+		// console.log('hits endpoint with: ' + bodyData.name, bodyData.email, bodyData.emailBody)
+
+		const mailjet = new Mailjet({
+			apiKey: process.env.MJ_APIKEY_PUBLIC,
+			apiSecret: process.env.MJ_APIKEY_PRIVATE
+		});
+
+		const request = mailjet
+			.post('send', { version: 'v3.1' })
+			.request({
+				Messages: [
+					{
+						From: {
+							Email: "rossepsteinforge@gmail.com",
+							Name: "Ross Epstein"
+						},
+						To: [
+							{
+								Email: "rossepsteinforge@gmail.com",
+								Name: "Ross Epstein"
+							}
+						],
+						Subject: "Celtic Mayhem Contact Form from sender " + bodyData.name,
+						TextPart: `You received a message from ${bodyData.name} with the email address ${bodyData.email}.
+						
+						  
+						The message says: ${bodyData.emailBody}`,
+						HTMLPart: `<h4>You received a message from ${bodyData.name} with the email address ${bodyData.email}.   
+						The message says: <br /> <br /> ${bodyData.emailBody}</h4>`
+					}
+				]
+			})
+
+		request
+			.then((result) => {
+				console.log(result.body);
+				return result;
+			})
+			.catch((err) => {
+				console.log(err.statusCode)
+		})
+
 
 
 		}
